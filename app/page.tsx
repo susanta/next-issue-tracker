a221b558dcb6a1d9ea74f994ef361643d62ef488
Susanta <inbox.susanta@gmail.com>
2023-12-26 15:25:54 +0530

    Build the new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { Button } from '@radix-ui/themes';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div>
-      <Button>New Issue</Button>
-    </div>
-  );
+  return <div>Next issue tracker</div>;
 }
