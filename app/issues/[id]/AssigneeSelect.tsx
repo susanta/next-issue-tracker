bef72c500504a0bf965efdb041d3f3ec7d876751
Susanta <inbox.susanta@gmail.com>
2024-01-06 11:06:43 +0530

    Refactor: AssigneeSelect Component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,42 +1,34 @@
 'use client';
 
+import Skeleton from '@/app/components/Skeleton';
 import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Skeleton from '@/app/components/Skeleton';
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then((res) => res.data),
-    staleTime: 60 * 1000, //60s
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId === 'unassigned' ? null : userId,
+      })
+      .catch(() => {
+        toast.error('Changes could not be saved.');
+      });
+  };
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || 'unassigned'}
-        onValueChange={(userId) => {
-          axios
-            .patch('/api/issues/' + issue.id, {
-              assignedToUserId: userId === 'unassigned' ? null : userId,
-            })
-            .catch(() => {
-              toast.error('Changes could not be saved.');
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -55,5 +47,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3,
+  });
 
 export default AssigneeSelect;
